f34f10b9151cb325a784fdef9434516b2fab09c8
jjyoummmin <dbwlals97@naver.com>
2021-03-09 00:01:38 +0900

    초기 템플릿 화면 설정

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('add');
+});
+
+module.exports = router;
